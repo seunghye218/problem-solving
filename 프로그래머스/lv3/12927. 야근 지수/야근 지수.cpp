a0487df8c6a58207afc661ba645235965695454b
seunghye218 <93831492+seunghye218@users.noreply.github.com>
2023-10-08 23:04:20 +0900

    [level 3] Title: 야근 지수, Time: 1.52 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
@@ -1,8 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#include <queue>
+#include <numeric>
 
 using namespace std;
 
@@ -16,13 +15,15 @@ long long   get_sum_power(const vector<int> &works) {
 
 // 내림차순 정렬 후 가장 큰 값들 중 가장 뒤에 있는 인덱스 부터 앞쪽으로 차감
 long long solution(int n, vector<int> works) {
+// 야근을 안해도 될 때.
+    if (accumulate(works.begin(), works.end(), 0) < n)
+        return 0;
     int i = 0;
     sort(works.rbegin(), works.rend());
     while (n) {
         while (i + 1 < works.size() && works[i] == works[i + 1])
             ++i;
         while (i != -1 && n) {
-            if (!works[i] && !i) return 0;
             if (works[i]) --works[i];
             --n;
             --i;

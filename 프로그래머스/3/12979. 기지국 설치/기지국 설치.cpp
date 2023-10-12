a44d39a24d8db740e1f93d9013de31fe30c5be12
seunghye218 <93831492+seunghye218@users.noreply.github.com>
2023-10-12 11:47:59 +0900

    [level 3] Title: 기지국 설치, Time: 0.09 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define APART_MAX 2147483647
+
+int solution(int n, vector<int> stations, int w) {
+    int answer = 0, range = (w << 1) + 1;
+//  add station end
+    stations.push_back(APART_MAX);
+    ++n;
+    for (int apart = 1, i = 0; apart < n;) {
+//         station 범위 전 - apart + w 에 설치 가정 후 범위 패스.
+        if (apart < stations[i] - w) {
+            apart += range;
+            ++answer;
+//         station 범위 안 - 전파 범위 1 칸 뒤로 패스.
+        } else {
+            apart = stations[i] + w + 1;
+            ++i;
+        }
+    }
+    return answer;
+}

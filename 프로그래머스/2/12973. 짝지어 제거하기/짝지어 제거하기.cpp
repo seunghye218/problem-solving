4afa7cf5de889fd365f0b200bfa548fb23890908
seunghye218 <93831492+seunghye218@users.noreply.github.com>
2023-10-16 22:07:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.01 ms, Memory: 6.46 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -5,12 +5,8 @@ using namespace std;
 
 int solution(string s) {
     stack<char> st;
-
-    for (int i = 0, len = s.size(); i < len; ++i) {
-        if (!st.empty() && st.top() == s[i])
-            st.pop();
-        else
-            st.push(s[i]);
-    }
-    return st.size() ? 0 : 1;
+    for (int i = 0, len = s.size(); i < len; ++i)
+        if (st.size() && st.top() == s[i]) st.pop();
+        else st.push(s[i]);
+    return st.empty();
 }

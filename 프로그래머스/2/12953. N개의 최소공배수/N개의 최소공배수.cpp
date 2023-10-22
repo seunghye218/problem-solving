19eb856b1f059c2cce6a8837594c0c30929fa2a2
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-10-22 21:38:02 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,51 +1,13 @@
 #include <vector>
-#include <map>
-#include <numeric>
-#include <cmath>
+#include <algorithm>
+
 using namespace std;
 
-#define MAX_NUM 100
-
-inline bool is_prime(const int &n);
-inline int cnt_prime_factor(int &n, const int &div);
-
-// 배열의 모든 원소를 소수로 나누며 1이 되면 erase
+int gcd(int x, int y) { return x % y == 0 ? y : gcd(y, x % y); }
+int lcm(int x, int y) { return x * y / gcd(x, y); }
 int solution(vector<int> arr) {
-    int i, j, answer = 1;
-    map<int, int>   prime_factors;
-    
-    while (arr.size()) {
-        for (i = 2; i < MAX_NUM; ++i) {
-            if (is_prime(i)) {
-                int &cnt_prime = prime_factors[i];
-                for (int j = 0; j < arr.size(); ++j) {
-                    cnt_prime = max(cnt_prime, cnt_prime_factor(arr[j], i));
-                    if (arr[j] == 1) {
-                        arr.erase(arr.begin() + j);
-                        --j;
-                    }
-                }
-            }
-        }
-    }
-    for (map<int, int>::iterator it = prime_factors.begin();
-        it != prime_factors.end(); ++it)
-            answer *= int(pow(it->first, it->second));
+    int answer = arr[0];
+    for (int i = 1; i < arr.size(); ++i)
+        answer = lcm(answer, arr[i]);
     return answer;
-}
-
-inline bool is_prime(const int &n) {
-    for (int i = 2; i * i <= n; ++i)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
-inline int cnt_prime_factor(int &n, const int &div) {
-    int cnt = 0;
-    while (n % div == 0) {
-        n /= div;
-        ++cnt;
-    }
-    return cnt;
 }

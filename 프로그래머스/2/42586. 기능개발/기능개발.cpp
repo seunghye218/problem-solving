158eeb20487bd1d181ab628113844d482089eaee
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-11-03 18:27:32 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    int deploy, day = 1;
+    int deploy, day;
     
     for (int i = 0; i < progresses.size(); i += deploy) {
         // 맨 앞 작업 처리
-        while (progresses[i] + speeds[i] * day < 100)
-            ++day;
+        day = (99 - progresses[i]) / speeds[i] + 1;
         deploy = 1;
         // 앞 작업 처리 중 처리된 뒷 작업
         while (i + deploy < progresses.size() && 

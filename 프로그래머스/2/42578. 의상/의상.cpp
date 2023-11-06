9850f1ad52b3a3b4d91934c9d14ebb7a571440f6
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-11-06 10:28:54 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+inline int factorial(int n) {
+    return n < 2 ? 1 : n * factorial(n-1);
+}
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    // 옷 종류(string)에 따른 가짓수(int)
+    unordered_map<string, int>  clothes_map;
+
+    for (int i = 0; i < clothes.size(); ++i)
+        ++clothes_map[clothes[i][1]];
+    // mul(옷 가짓수 + 1) - 1
+    for (auto it = clothes_map.begin(); it != clothes_map.end(); ++it)
+        answer *= (it->second + 1);
+    --answer;
+    return answer;
+}
+
+

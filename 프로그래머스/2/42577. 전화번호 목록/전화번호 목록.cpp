5d4d093522cb2429a8817b8f80b6bc70879c98eb
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-11-11 23:20:13 +0900

    [level 2] Title: 전화번호 목록, Time: 82.05 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -6,14 +6,8 @@ using namespace std;
 
 bool solution(vector<string> pb) {
     sort(pb.begin(), pb.end());
-    unordered_set<string> pb_set;
-    pb_set.insert(pb.begin(), pb.end());
-    for (int i = pb.size()-1; i > 0; --i) {
-        auto it = pb_set.find(pb[i]);
-        string str(*it);
-        pb_set.erase(it);
-        for (int j = 1; j < str.size(); ++j)
-            if (pb_set.find(str.substr(0,j)) != pb_set.end())                  return false;
-    }
+    for (int i = 1; i < pb.size(); ++i)
+        if (pb[i - 1] == pb[i].substr(0, pb[i - 1].size()))
+            return false;
     return true;
 }

bee9981c96b67a56c2bc2558e9c92131385b3290
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-11-28 20:13:17 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.04 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, int k) {
+    vector<int> answer;
+    for (int i = k; i <= n; i+=k)
+        answer.push_back(i);
+    return answer;
+}

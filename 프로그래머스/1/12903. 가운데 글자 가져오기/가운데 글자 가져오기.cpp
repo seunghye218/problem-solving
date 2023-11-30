93d60e0b255e6a0bc6db92ae7176898dcd6bc638
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-11-30 21:42:27 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+string solution(string s) {
+    int half_len = (s.size() - 1) / 2;
+    if ((s.size() & 1) == 1)
+        return s.substr(half_len, 1);
+    else
+        return s.substr(half_len , 2);
+}

256c1cb9f6c5a8f964850db0cbe5a1a5f932c224
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-12-17 00:15:54 +0900

    [level 2] Title: 주식가격, Time: 15.30 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,25 +1,16 @@
 #include <vector>
 #include <stack>
-#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size(), 0);
     stack<int> st; // index 저장
     for (int i = 0; i < prices.size(); ++i) {
-        if (st.empty()) {
-            st.push(i);
-        } else {
-            if (prices[i] >= prices[st.top()]) { // 값이 커지면 push
-                st.push(i);
-            } else { // 값이 작다면 현재 index - 저장 index 반복
-                while (!st.empty() && prices[st.top()] > prices[i]) {
-                    answer[st.top()] = i - st.top();
-                    st.pop();
-                }
-                st.push(i);
-            }
+        while (!st.empty() && prices[st.top()] > prices[i]) {
+            answer[st.top()] = i - st.top();
+            st.pop();
         }
+        st.push(i);
     }
     while (!st.empty()) {
         answer[st.top()] = prices.size() - 1 - st.top();

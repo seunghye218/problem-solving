867ec225788610d78745e323afa622ebdbdb5b45
seunghye <93831492+seunghye218@users.noreply.github.com>
2023-12-18 22:52:59 +0900

    [level unrated] Title: rny_string, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+string solution(string rny_string) {
+    string answer = "";
+    for (char c : rny_string) {
+        if (c == 'm') {
+            answer += "rn";
+        } else {
+            answer.push_back(c);
+        }
+    }
+    return answer;
+}

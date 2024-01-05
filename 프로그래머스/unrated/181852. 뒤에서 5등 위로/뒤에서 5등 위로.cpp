b3a968760d05e3b8337764b8a69447ddeba667ff
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-01-05 17:26:00 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,8 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(), num_list.end());
+    return vector<int>(num_list.begin() + 5, num_list.end());
+}

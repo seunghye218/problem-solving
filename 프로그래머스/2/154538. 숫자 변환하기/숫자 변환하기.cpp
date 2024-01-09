33bc336a2e568523d139e73e822a9d6fed2cdce0
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-01-09 16:14:59 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.94 ms, Memory: 6.18 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -2,35 +2,27 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1000001];
+int dp[1000001];
 
 struct dx {
     int d, x;
 };
 
 int solution(int x, int y, int n) {
-    int answer = 987654321;
     queue<dx> q;
-    
     q.push({0, x});
     while (!q.empty()) {
         dx d_x = q.front();
         q.pop();
         if (d_x.x == y)
             return d_x.d;
-        if (d_x.x > y)
+        if (d_x.x > y || (dp[d_x.x] && d_x.d > dp[d_x.x]))
             continue;
-        if (arr[d_x.x])
-            if (d_x.d < arr[d_x.x])
-                arr[d_x.x] = d_x.d;
-            else
-                continue;  
-        else
-            arr[d_x.x] = d_x.d;
+        dp[d_x.x] = d_x.d;
         ++d_x.d;
         q.push({d_x.d, d_x.x * 3});
         q.push({d_x.d, d_x.x << 1});
         q.push({d_x.d, d_x.x + n});
     }
-    return answer == 987654321 ? -1 : answer;
+    return -1;
 }

f42ad15431d296d1023f70cf87b5f5aa70968a8e
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-01-17 13:11:18 +0900

    [level Lv.2] Title: 2개 이하로 다른 비트, Time: 29.16 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/Lv.2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,24 +1,12 @@
 #include <vector>
-#include <bitset>
 using namespace std;
 
 vector<long long> solution(vector<long long> numbers) {
     for (long long &n : numbers) {
-        bitset<64> bs(n);
-        if (n < 2) {
-            ++n; continue;
-        }
-        int top_true = 63, bottom_false = 0;
-        while (!bs[top_true])
-            --top_true;
-        while (bs[bottom_false])
-            ++bottom_false;
-        bs.set(bottom_false);
-        if (bottom_false) {
-            while (--bottom_false && !bs[bottom_false]);
-            bs.reset(bottom_false);
-        }
-        n = bs.to_ullong();
+        long long bit = 1;
+        while (n & bit) 
+            bit <<= 1;
+        n = (n | bit) & ~(bit >> 1);
     }
     return numbers;
 }

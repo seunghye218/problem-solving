2dfa714ce5edfb71b5c951cf12414e3f98400ac6
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-02-13 23:05:52 +0900

    [level 2] Title: 택배상자, Time: 10.86 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -4,24 +4,14 @@
 using namespace std;
 
 int solution(vector<int> order) {
-    queue<int> container;
-    stack<int> second;
-    for (int i = 1; i <= order.size(); ++i)
-        container.push(i);
-    
     int answer = 0;
-    for (int n : order) {
-        while (!container.empty() && n > container.front()) {
-            second.push(container.front());
-            container.pop();
+    stack<int> st;
+    for(int i = 1; i <= order.size(); ++i) {
+        st.push(i);
+        while(!st.empty() && order[answer] == st.top()) {
+            st.pop();
+            answer++;
         }
-        if (n != container.front() && n != second.top())
-            break;
-        if (n == container.front())
-            container.pop();
-        else
-            second.pop();
-        ++answer;
     }
     return answer;
 }

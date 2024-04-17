db763c00649e96cf4176087b18fc35a87c64567e
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-04-17 09:58:35 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a * b);
+    }
+}

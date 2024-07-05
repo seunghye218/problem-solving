83d79bc82ccae4945b5b039a4747fc91f1fdab81
seunghye <93831492+seunghye218@users.noreply.github.com>
2024-07-05 11:01:52 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,20 +5,19 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String str = scanner.nextLine();
-
         int[] arr = new int[128];
-
-        for (char c : str.toCharArray()) {
-            ++arr[c];
+        
+        String word = scanner.nextLine();
+        for (char c: word.toCharArray()) {
+            arr[c]++;
         }
-
-        StringBuilder out = new StringBuilder();
-        for (byte c = 'a'; c <= 'z'; ++c) {
-            out.append(arr[c]);
-            out.append(' ');
+        
+        StringBuilder s = new StringBuilder();
+        for (char c = 'a'; c <= 'z'; ++c) {
+            s.append(arr[c]);
+            s.append(' ');
         }
-
-        System.out.println(out);
+        s.delete(s.length() - 1, s.length());
+        System.out.println(s);
     }
 }
